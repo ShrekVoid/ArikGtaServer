e675d363bbf4c93c0f6f3ccf216e370622578a41
ARIK-PC\Arik <furmanmail@gmail.com>
2017-05-30 15:17:07 +0300

    Add project files.

---
+++ coordenates/Coordenates.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+using System.IO;
+
+namespace newcommands
+{
+    public class newcommands : Script
+    {
+        [Command("coords")]
+        public void coords(Client player, string coordName)
+        {
+            Vector3 playerPosGet = API.getEntityPosition(player);
+            var pPosX = (playerPosGet.X.ToString().Replace(',', '.') + ", ");
+            var pPosY = (playerPosGet.Y.ToString().Replace(',', '.') + ", ");
+            var pPosZ = (playerPosGet.Z.ToString().Replace(',', '.'));
+            Vector3 playerRotGet = API.getEntityRotation(player);
+            var pRotX = (playerRotGet.X.ToString().Replace(',', '.') + ", ");
+            var pRotY = (playerRotGet.Y.ToString().Replace(',', '.') + ", ");
+            var pRotZ = (playerRotGet.Z.ToString().Replace(',', '.'));
+
+            API.sendChatMessageToPlayer(player, "Your position is: ~y~" + playerPosGet, "~w~Your rotation is: ~y~" + playerRotGet);
+            StreamWriter coordsFile;
+            if (!File.Exists("SavedCoords.txt"))
+            {
+                coordsFile = new StreamWriter("SavedCoords.txt");
+            }
+            else
+            {
+                coordsFile = File.AppendText("SavedCoords.txt");
+            }
+            API.sendChatMessageToPlayer(player, "~r~Coordinates have been saved!");
+            coordsFile.WriteLine("| " + coordName + " | " + "Saved Coordenates: " + pPosX + pPosY + pPosZ + " Saved Rotation: " + pRotX + pRotY + pRotZ);
+            coordsFile.Close();
+        }
+        [Command("savedcoords")]
+        public void savedCoords(Client player)
+        {
+            API.sendChatMessageToPlayer(player, "~r~Current Saved Coordenates:");
+            int counter = 0;
+            string coordsLine;
+            System.IO.StreamReader file = new System.IO.StreamReader("SavedCoords.txt");
+            while ((coordsLine = file.ReadLine()) != null)
+            {
+                API.sendChatMessageToPlayer(player, coordsLine);
+                counter++;
+            }
+            file.Close();
+        }
+    }
+}
