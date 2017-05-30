b402b82761db92172d392ce30628a1dcc017f069
ARIK-PC\Arik <furmanmail@gmail.com>
2017-05-30 17:25:15 +0300

    Initial Commit

---
+++ ArikServer/ClientManager.js
@@ -0,0 +1,17 @@
+/// <reference path="types-gtanetwork/index.d.ts" />
+"use strict";
+var mainCam = null;
+API.onServerEventTrigger.connect(function (name, args) {
+    switch (name) {
+        case "selection_start":
+            mainCam = API.createCamera(new Vector3(-83.23415, -835.2518, 326.6785), new Vector3());
+            API.pointCameraAtEntity(mainCam, API.getLocalPlayer(), new Vector3());
+            API.setActiveCamera(mainCam);
+            break;
+        default:
+            break;
+    }
+});
+API.onUpdate.connect(function () {
+    API.disableAllControlsThisFrame();
+});
---
+++ ArikServer/ServerManager.cs
@@ -0,0 +1,55 @@
+﻿using GTANetworkServer;
+using GTANetworkShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArikServer
+{
+    public class ServerManager : Script
+    {
+        private bool[] dimensions;
+        private const int MainDimension = 0;
+
+        public ServerManager()
+        {
+			Console.WriteLine("Hello");
+            dimensions = new bool[API.getMaxPlayers() + 1];
+            dimensions[MainDimension] = true;
+            API.onPlayerFinishedDownload += API_onPlayerFinishedDownload;
+            API.onClientEventTrigger += API_onClientEventTrigger;
+			Console.WriteLine("Im uP");
+        }
+
+        private void API_onClientEventTrigger(Client sender, string eventName, params object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void API_onPlayerFinishedDownload(Client player)
+        {
+			Console.WriteLine("Someone joined");
+            int dimension = GetEmptyDimension();
+            dimensions[dimension] = true;
+
+            API.setEntityDimension(player, dimension);
+            API.freezePlayer(player, true);
+
+            API.setEntityPosition(player, new Vector3(-81.12128, -824.6755, 326.084));
+            API.setEntityRotation(player, new Vector3(0, 0, 154.1422));
+
+            string msg = "~o~Welcome to Arik's test server";
+            
+            API.sendChatMessageToPlayer(player, msg);
+            API.sendNotificationToAll(player.name + " has joined the server.");
+            API.triggerClientEvent(player, "selection_start");
+        }
+
+        private int GetEmptyDimension()
+        {
+            return Array.IndexOf(dimensions, false);            
+        }
+    }
+}
